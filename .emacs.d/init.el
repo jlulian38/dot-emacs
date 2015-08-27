c12b7eb2c72543b1f5c91b92f6952c37258215a4
Julian R. Moore <jreedmoore@gmail.com>
2015-08-26 20:13:09 -0400

    Add .emacs.d with autoinstall and smart-parens

---
+++ .emacs.d/init.el
@@ -0,0 +1,24 @@
+(require 'package)
+(require 'cl-lib)
+
+;; Require packages
+(setq my-pkgs '(cider better-defaults idle-highlight-mode
+		      ido-ubiquitous magit smex zenburn-theme rainbow-delimiters smartparens))
+
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(setq pkgs-to-install (cl-remove-if 'package-installed-p my-pkgs))
+
+(when (> (length pkgs-to-install) 0)
+  (package-refresh-contents)
+  (dolist (pkg pkgs-to-install)
+      (package-install pkg)))
+
+;; Configuration
+
+(load-theme `zenburn t)
+
+(add-hook `prog-mode-hook #'rainbow-delimiters-mode)
+
+(mapc 'load (directory-files "~/.emacs.d/lib" t "^[^#].*el$"))
